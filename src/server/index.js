06be0c1f8ad183e33104f6ed4d2340bc988f2de3
Knowledge Brown <knowledgexbrown@gmail.com>
2019-04-10 16:20:56 -0500

    Server up

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,38 @@
-console.log("foo");
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
+
+const app = express();
+
+app.store = session({
+    name: "session",
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        path: "/"
+    }
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(app.store);
+
+//test
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+    console.log('Knowledge was here')
+});
