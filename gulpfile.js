240b4fdb5d7a0bab487f5da38ad5e878e57684f0
Knowledge Brown <knowledgexbrown@gmail.com>
2019-08-13 13:59:48 -0500

    New build config pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,36 @@
-const browserSync = ('browser-sync'),
-    webpack = require('webpack'),
-    webpackDevMiddleWare = require('webpack-dev-middleware'),
-    webpackHotMiddleWare = require('webpack-hot-middleware'),
-    pug = require('pug');
+const browserSync = require('browser-sync').create(),
+    webpack = require('webpack'), 
+    gulp = require('gulp');
 const webpackSettings = require('./webpack.config');
-const bundler = webpack(webpackSettings);
-const templatePath = path.join(__dirname, '/src/*.pug');
 
-gulp.task('pug', () => {
-    let locals = 'Some message';
-    return gulp.src(templatePath)
-        .pipe(pug({
-            locals: locals
-        }))
-        .pipe(gulp.dest(path.join(__dirname('/public'))));
-});
-
-gulp.task('default', ['pug'], () => {
-    browserSync.init({
-        ui: {
-            port: 8080
-        },
-        server: {
-            baseDir: path.join(__dirname, '/src/client'),
-            middleware: [
-                webpackDevMiddleWare(bundler, {
-                    publicPath: templatePath,
-                    stats: {color: true}
-                }),
-                webpackHotMiddleWare(bundler)
-            ]
-        },
-        files: [path.join(__dirname, '/src/client/*.js')],
-        logFileChanges: true,
-        logConnections: true
+const assets = () => new Promise((resolve, reject) => {
+    webpack(webpackSettings, (err, stats) => {
+        if (err){
+            reject(err);
+        }
+        if (stats.hasErrors()){
+            reject(new Error(stats.compilation.errors.join('\n')));
+        }
+        resolve();
     });
-
 });
+exports.assets = assets;
+
+const serve = callback => {
+    browserSync.init({
+        server: './public',
+        port: 8080,
+        host: 'localhost'
+    }, callback);
+};
+exports.serve = serve;
+
+const reload = callback => {
+    browserSync.reload();
+    callback();
+};
+exports.reload = reload;
+
+const watch = callback => gulp.watch('src/client/*.js', gulp.series(assets, reload));
+exports.watch = watch;
+exports.develop = gulp.series(assets, serve, watch);
